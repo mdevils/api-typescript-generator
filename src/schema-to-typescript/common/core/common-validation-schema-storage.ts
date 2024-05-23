6994c8b8600f1c6b84523f0f16dfc37e6682e675
Marat Dulin <mdevils@gmail.com>
2024-05-23 10:33:38 +0200

    fix: improve mediaType condition for validators

--- src/schema-to-typescript/common/core/common-validation-schema-storage.ts
+++ src/schema-to-typescript/common/core/common-validation-schema-storage.ts
@@ -67,7 +67,7 @@ export class CommonValidationSchemaStorage<T> {
             if (!this.schemas[key]) {
                 throw new Error(`Schema with key "${key}" not found`);
             }
-            if (data.mediaType && isJsonMediaType(data.mediaType)) {
+            if (data.mediaType !== undefined && isJsonMediaType(data.mediaType)) {
                 try {
                     this.assertDataShape(this.schemas[key], data);
                 } catch (e) {
